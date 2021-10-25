07b7a0a3fdbeb731fa51d794ef0c6d1e53a9133a
m3xq <z3w1l0q@protonmail.com>
2021-10-25 13:42:04 -0500

    Change config approach to module builder

--- lib/ez_attributes.rb
+++ lib/ez_attributes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'ez_attributes/config'
-
 # Easily define initializers with keyword args.
 # It supports required and optional args.
 #
@@ -16,45 +14,49 @@ require_relative 'ez_attributes/config'
 #   User.new(name: 'Matheus', age: 22)
 #   => #<User:0x000055bac152f130 @name="Matheus", @age=22, @email="guest@user.com">
 module EzAttributes
-  class << self
-    attr_accessor :config
-
-    # Defines config
-    def configure(**opts)
-      @config = Config.new(**opts)
-      self
-    end
-  end
   # Gem version
-  VERSION = '0.3.0'
+  VERSION = "0.3.0"
 
   # Attributes that won't have a getter to prevent conflicts with default methods
   EXCEPTIONS = [:class].freeze
 
-  # Defines multiple keyword arguments for a class initializer
-  def attributes(*args, **args_with_default)
-    required_args = args.map { |name| "#{name}:" }
-    optional_args = args_with_default.map { |name, _| "#{name}: __args_with_default[:#{name}]" }
-    init_args = (required_args + optional_args).join(', ')
-
-    define_method('__args_with_default', -> { Marshal.load(Marshal.dump(args_with_default)) })
-    private :__args_with_default
-
-    all_args = args + args_with_default.keys
-    config = EzAttributes.config
-    attr_reader(*(all_args - EXCEPTIONS)) if config&.getters || config.nil?
-
-    class_eval <<~RUBY, __FILE__, __LINE__ + 1
-      def initialize(#{init_args})
-        #{all_args.map { |name| "@#{name} = binding.local_variable_get(:#{name})" }.join('; ')}
-      end
-    RUBY
-    # Reset config
-    EzAttributes.config = Config.new
+  def self.extended(other)
+    other.extend configure(**default_args)
   end
 
-  # Defines a single keyword argument for a class initializer
-  def attribute(name)
-    attributes(name)
+  def self.configure(**opts)
+    mod = Module.new
+
+    mod.module_eval do
+      # Defines multiple keyword arguments for a class initializer
+      define_method :attributes do |*args, **args_with_default|
+        required_args = args.map { |name| "#{name}:" }
+        optional_args = args_with_default.map { |name, _| "#{name}: __args_with_default[:#{name}]" }
+        init_args = (required_args + optional_args).join(", ")
+
+        define_method("__args_with_default", -> { Marshal.load(Marshal.dump(args_with_default)) })
+        private :__args_with_default
+
+        all_args = args + args_with_default.keys
+        attr_reader(*(all_args - EXCEPTIONS)) if opts[:getters]
+
+        class_eval <<~RUBY, __FILE__, __LINE__ + 1
+          def initialize(#{init_args})
+            #{all_args.map { |name| "@#{name} = binding.local_variable_get(:#{name})" }.join("; ")}
+          end
+        RUBY
+      end
+
+      # Defines a single keyword argument for a class initializer
+      define_method :attribute do |name|
+        attributes(name)
+      end
+    end
+
+    mod
+  end
+
+  def self.default_args
+    {getters: true}
   end
 end
